caa7bf83cb2bb28535aa3058528ed6995fc84a7e
Joaquin Delgado <joaquin.delgado.lorino@gmail.com>
2026-09-05 14:27:22 -0400

    Add backend deployment variable

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,2 +1,4 @@
-export const SERVER_URL = 'http://localhost:3001';
+export const SERVER_URL =
+  import.meta.env.VITE_API_URL || 'http://localhost:3001';
+
 export const API_URL = `${SERVER_URL}/api`;
